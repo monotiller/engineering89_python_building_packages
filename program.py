358dab9b4342773e94fbd035bcee3545f6fe635e
jrmswell <18310129+monotiller@users.noreply.github.com>
2021-06-29 11:53:54 +0100

    Update program.py

--- program.py
+++ program.py
@@ -1,5 +1,9 @@
 from app.complicated_calc import ComplicatedCalc
 
 divide_numbers = ComplicatedCalc.divisible(3, 2)
+modulous_numbers = ComplicatedCalc.modulus(2, 10)
+positive_numbers = ComplicatedCalc.positive(-2)
 
 print(divide_numbers.calc_list)
+print(modulous_numbers.calc_list)
+print(positive_numbers.calc_list)
